6243de73df558361113a5d651724078eb4f7972c
Ngabonziza Nestor <ngabonest@gmail.com>
2023-05-31 10:58:32 +0300

    fix:changing the Link into a button in footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { changeCategory } from "../../features/newsSlice";
 import { CATEGORIES, LINKS } from "../../assets/data";
 
 const Footer = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleClick = (value: string) => {
+    dispatch(changeCategory(value));
+    navigate("/");
+    // window.scroll(0, 0);
+  };
   return (
     <div className="bg-[#2E2F41] py-4 text-gray-200">
       <div className="max-w-7xl mx-auto">
@@ -17,12 +23,9 @@ const Footer = () => {
             `}
                 key={categoryObject.id}
               >
-                <Link
-                  to="/"
-                  onClick={() => dispatch(changeCategory(categoryObject.value))}
-                >
+                <button onClick={() => handleClick(categoryObject.value)}>
                   {categoryObject.name}
-                </Link>
+                </button>
               </li>
             ))}
           </ul>
@@ -30,9 +33,7 @@ const Footer = () => {
           <ul className=" flex flex-col md:flex-row md:items-center gap-12 my-4 px-8 py-4">
             {LINKS.map((item, index) => (
               <li className="hover:text-blue-300 hover:underline" key={index}>
-                <Link className="" to={item.link}>
-                  {item.title}
-                </Link>
+                <Link to={item.link}>{item.title}</Link>
               </li>
             ))}
           </ul>
